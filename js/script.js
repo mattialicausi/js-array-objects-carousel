b546d544a7a7e5e75136d5ea6240f12522c47e45
mattialicausi <licausivitomattia@gmail.com>
2022-10-21 17:30:29 +0200

    creato tre funzioni map per creare tre array con i 3 valori di array image distinti

--- js/script.js
+++ js/script.js
@@ -48,31 +48,31 @@ Aggiungere il **ciclo infinito** del carosello. Ovvero se la miniatura attiva è
 //ARRAY IMG
 const images = [
     {
-        url: 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
-        title: 'Svezia',
-        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+        'url': 'http://www.viaggiareonline.it/wp-content/uploads/2014/11/sweden_148857365.jpg',
+        'title': 'Svezia',
+        'description': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
     },
 
     {
-        url: 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg',
-        title: 'Perù',
-        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+        'url': 'https://static1.evcdn.net/images/reduction/1513757_w-1920_h-1080_q-70_m-crop.jpg',
+        'title': 'Perù',
+        'description': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
     },
 
     {
-        url: 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c',
-        title: 'Chile',
-        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+        'url': 'https://img.itinari.com/pages/images/original/0d3ed180-d22d-48e8-84df-19c4d888b41f-62-crop.jpg?ch=DPR&dpr=2.625&w=1600&s=7ebd4b5a9e045f41b4e0c7c75d298d6c',
+        'title': 'Chile',
+        'description': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
     },
     {
-        url: 'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg',
-        title: 'Argentina',
-        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+        'url': 'https://static1.evcdn.net/images/reduction/1583177_w-1920_h-1080_q-70_m-crop.jpg',
+        'title': 'Argentina',
+        'description': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
     },
     {
-        url: 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop',
-        title: 'Colombia',
-        description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
+       ' url': 'https://cdn.sanity.io/images/24oxpx4s/prod/ed09eff0362396772ad50ec3bfb728d332eb1c30-3200x2125.jpg?w=1600&h=1063&fit=crop',
+        'title': 'Colombia',
+        'description': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam.'
     },
 ];
 
@@ -84,26 +84,34 @@ const contenitorePadre = document.getElementById('contenitore-padre');
 // funzione per visualizzare valori e indici dell'array in console
 images.forEach(stampafoto);
     function stampafoto(valori){
-        //console.log(valori.url);
-        //console.log(valori.title);
-        //console.log(valori.description);
+        // console.log(valori.url);
+        // console.log(valori.title);
+        // console.log(valori.description);
     }
 
 //funzione per ciclare gli elementi dell'array
-//function ciclaArray(){
+function ciclaArray(){
     images.forEach(ciclaInfo);
     function ciclaInfo(valoriInfo){
         let linkFoto = valoriInfo.url;
         let titolo = valoriInfo.title;
         let descrizioneFoto = valoriInfo.description;
-       
-      
-    }
-//}
+    }  
+}
+
+//funzione per ciclare array ed inserire elementi in un nuovo array
+const arrayUrl = images.map((value) => value.url);
+console.log(arrayUrl);
+
+const arrayTitle = images.map((value) => value.title);
+console.log(arrayTitle);
+
+const arrayDescription = images.map((value) => value.description);
+console.log(arrayDescription);
+
 
 // funzione per creare layout dimanico
 function creaLayout(){
-    
     const layoutCreato = `
     <div id="my-container">
     <div class="contenitore-generale">
@@ -146,5 +154,5 @@ function creaLayout(){
 }
 
 // RICHIAMO FUNZIONI
-creaLayout();
-//ciclaArray()
+
+ciclaArray()
